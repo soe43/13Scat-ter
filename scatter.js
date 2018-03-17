91325a5f734acce2998b932f29e7b858aca36ea2
soe43 <soe433@gmail.com>
2018-03-17 01:03:35 -0400

    added x-axis labels and created two axes

--- scatter.js
+++ scatter.js
@@ -7,6 +7,30 @@ for(i=0;i<10;i++){
     data.push([x[i],y[i]])
 }
 
+//Creating Labels for x axis
+var xAxisLabels = [];
+for(var i = 2; i<= 24; i += 2){
+    xAxisLabels.push(i);
+}
+
+//Adding x-axis labels to svg
+d3.select("#screen").selectAll("text").data(xAxisLabels).enter().append("text").attr("x", function(d, i){
+    return (i+1) * 700 / xAxisLabels.length;
+}).attr("y", 600).text(function(d){ return d; });
+
+//Creating Labels for y axis
+var yAxisLabels = [];
+for(var i = 100; i<= 500; i+=100){
+    yAxisLabels.push(i);
+}
+
+//Adding y-axis labels to svg
+d3.select("#screen").selectAll("text").data(yAxisLabels).enter().append("text").attr("x", 0).attr("y", function(d,i){
+    return (i+1) * 600 / yAxisLabels.length;
+}).text(function(d) { return d; });
+
+d3.select("#screen").selectAll("text").data(yAxisLabels).enter()
+    
 var container=d3.select("svg");
 var circles=container.selectAll("circle").data(data).enter();
 circles.append("circle").attr("cx", function(d) {return (d[0]-10)*100/2;}).attr("cy", function(d) {return 600-d[1];}).attr("r", 10);
